5d83125fda84025cd9ad07ba3558ca3b2156d9ef
Estelle-Rozee <rozee.estelle@gmail.com>
2022-09-20 15:31:14 +0200

    feat: login component static  integration

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,7 @@
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Main from '../Main/Main';
+import Login from '../Login/Login';
 import './styles.scss';
 
 // == Composant
