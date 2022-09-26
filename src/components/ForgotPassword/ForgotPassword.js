56d0fd899c7b14c3230c5f846878a191f89a5aec
Estelle-Rozee <rozee.estelle@gmail.com>
2022-09-26 16:41:01 +0200

    feat: try with reCAPTCHA to be continued

--- src/components/ForgotPassword/ForgotPassword.js
+++ src/components/ForgotPassword/ForgotPassword.js
@@ -1,7 +1,11 @@
 import './ForgotPassword.scss';
+import ReCAPTCHA from 'react-google-recaptcha';
 import SeparationBar from '../SeparationBar/SeparationBar';
 
 function ForgotPassword() {
+  function onChange(value) {
+    console.log('Captcha value:', value);
+  }
   return (
     <div className="ForgotPassword">
       <h1 className="ForgotPassword-title">Mot de passe oublié ?</h1>
@@ -10,11 +14,17 @@ function ForgotPassword() {
         <form className="ForgotPassword-form">
           <label htmlFor="email" className="ForgotPassword-form-label">EMAIL (*)</label>
           <input id="email" type="email" className="ForgotPassword-form-input" placeholder="Votre email" />
+          <ReCAPTCHA
+            sitekey="6LexsTAiAAAAAPsJr53ngFrh2sE50_5T0bZdiD95"
+            onChange={onChange}
+          />
+          <div className="PostGenerateButton">
+            <button type="button" className="PostGenerateButton--button">
+              DEMANDER UN NOUVEAU MOT DE PASSE
+            </button>
+          </div>
         </form>
       </div>
-      <div className="PostGenerateButton">
-      <button type="button" className="PostGenerateButton--button">DEMANDER UN NOUVEAU MOT DE PASSE</button>
-    </div>
     </div>
   );
 }
