0673dc4a86224a55a6038371851c4633f6b411cc
Loicpicart <loic.picart78@gmail.com>
2022-09-26 15:29:35 +0200

    feat: PostComponant In Progress

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,21 +1,11 @@
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import Main from '../Main/Main';
-import Login from '../Login/Login';
-import Register from '../Register/Register';
-import AboutUs from '../AboutUs/AboutUs';
+import Home from '../Pages/Home';
 import './styles.scss';
 
 // == Composant
 function App() {
   return (
     <div className="app">
-      <Header />
-      {/* <Login /> */}
-      {/* <Register /> */}
-      <AboutUs />
-      {/* <Main /> */}
-      <Footer />
+      <Home />
     </div>
   );
 }
--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -5,22 +5,36 @@ function PostCard() {
   return (
     <div className="PostCard">
       <div className="PostCard__container">
-        <section className="PostCard__container--intro" id="1">La Créativité</section>
-        <section className="PostCard__container--body" id="1">C'est l'intelligence</section>
-        <section className="PostCard__container--conclusion" id="1">Qui s'amuse</section>
-        <img src={logo} alt="Copy" />
+        <div className="PostCard__container__content">
+          <section className="PostCard__container__content--intro" id="1">Certains débats entre dev’ font couler bcp d’encre virtuelle… Parmi les plus connus, 2 ou 4 espaces pour l’indentation, Javascript ou PHP, faut-il utiliser Redux avec React, ou encore, doit-on créer nos propres Hook avec React ? Chacun a son avis. De grandes et petites questions qui s’affrontent pour l’amour du code 😊 Voyons ça de plus près…</section>
+          <section className="PostCard__container__content--body" id="1">Je commence par le point le plus important de tous. Le choix du langage évidemment !
+            PHP ou Javascript, les 2 sont clairement très demandés en entreprise.
+            Alors, si vous voulez approfondir vos
+            connaissances dans l’un des 2,
+            lequel devriez-vous choisir ? Ou pour choisir votre premier langage, lequel privilégier?
+            PHP ou Javascript, les 2 sont clairement très demandés en entreprise.
+            connaissances dans l’un des 2,
+            lequel devriez-vous choisir ? Ou pour choisir votre premier langage, lequel privilégier?
+            PHP ou Javascript, les 2 sont clairement très demandés en entreprise.
+            connaissances dans l’un des 2,
+            lequel devriez-vous choisir ? Ou pour choisir votre premier langage, lequel privilégier?
+            PHP ou Javascript, les 2 sont clairement très demandés en entreprise.
+          </section>
+          <section className="PostCard__container__content--conclusion" id="1">Comme il peu probable que vous meniez votre carrière de développeur sans toucher aux deux solutions, et que le débat déchaine les passions, je vous laisse vous exprimer en commentaire ↓</section>
+        </div>
+        <img className="PostCard__container--copyLogo" src={logo} alt="Copy" />
       </div>
       <div className="PostCard__container">
         <section className="PostCard__container--intro" id="2">La Créativité</section>
         <section className="PostCard__container--body" id="2">C'est l'intelligence</section>
         <section className="PostCard__container--conclusion" id="2">Qui s'amuse</section>
-        <img src={logo} alt="Copy" />
+        <img className="PostCard__container--copyLogo" src={logo} alt="Copy" />
       </div>
       <div className="PostCard__container">
         <section className="PostCard__container--intro" id="3">La Créativité</section>
         <section className="PostCard__container--body" id="3">C'est l'intelligence</section>
         <section className="PostCard__container--conclusion" id="3">Qui s'amuse</section>
-        <img src={logo} alt="Copy" />
+        <img className="PostCard__container--copyLogo" src={logo} alt="Copy" />
       </div>
     </div>
   );
