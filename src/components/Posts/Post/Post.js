5ff85958624d74a8558e401c9f7a84e14b10285a
Loicpicart <loic.picart78@gmail.com>
2022-09-30 17:47:32 +0200

    Feat: CopyToClipboard Day2

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -1,22 +1,10 @@
 import PropTypes from 'prop-types';
-import React from 'react';
-import { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import './Post.scss';
 import { CopyToClipBoard } from 'react-copy-to-clipboard';
 
 function PostCard({ generatedPost }) {
   const postRef = useRef(null);
-  const [value, setValue] = useState('');
-  const [copied, setCopied] = useState(false);
-
-  const onChange = React.useCallback(({ target: { value } }) => {
-    setValue(value);
-    setCopied(copied);
-  }, []);
-
-  const onCopy = React.useCallback(() => {
-    setCopied(true);
-  }, []);
 
   useEffect(() => {
     postRef.current.scrollIntoView({ behavior: 'smooth' });
@@ -27,12 +15,10 @@ function PostCard({ generatedPost }) {
       <div className="PostCard__container">
         <div className="PostCard__container__content" ref={postRef}>
           <div className="PostCard__container__content--contain">
-            <p className="PostCard__container__content--text" type="text" value={value} onChange={onChange}>{generatedPost.introduction.content}</p>
+            <p className="PostCard__container__content--text" type="text">{generatedPost.introduction.content}</p>
             <p className="PostCard__container__content--text">{generatedPost.body.content}</p>
             <p className="PostCard__container__content--text">{generatedPost.conclusion.content}</p>
-            <CopyToClipBoard onCopy={onCopy} text={value}>
-              <button className="PostCard__container__content--copyBtn" type="button">Copier</button>
-            </CopyToClipBoard>
+            <button className="PostCard__container__content--copyBtn" type="button">Copier</button>
           </div>
         </div>
       </div>
