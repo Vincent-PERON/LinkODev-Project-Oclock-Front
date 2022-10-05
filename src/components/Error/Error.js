7bd63b5718a0e2e50125a8dc2544f922b2d9785d
Estelle-Rozee <rozee.estelle@gmail.com>
2022-10-05 11:49:13 +0200

    feat : first version of page 404

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,11 +1,20 @@
+import { Link, NavLink } from 'react-router-dom';
 import './Error.scss';
 
 function Error() {
   return (
     <div className="Error">
-      test page 404
+      <h1 className="Error-title">OUPS </h1>
+      <div className="Error-content">
+        <h2 className="Error-subtitle">Rien à voir ici !</h2>
+        <p className="Error-description">Pour retourner à l'accueil</p>
+        <NavLink className="Error-redirect" to="/">C'est par là !</NavLink>
+        <div className="Error-redirect-generator">
+          <NavLink className="Error-redirect-generator" to="/generator">Sinon tu peux générer un post digne de Chuck Norris</NavLink>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Error;
