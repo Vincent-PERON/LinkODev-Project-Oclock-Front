e828b0bed1985cdcd9e23df6efca87d41326dc59
Vincent PERON <peronvincen@gmail.com>
2022-10-06 17:24:44 +0200

    fix: header buttons if logged or not

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import { Link, NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-import logo from '../../assets/Images/Logo_3.png';
+import logo from 'src/assets/Images/Logo_3.png';
 
 import ConnexionButton from 'src/components/Buttons/ConnexionButton/ConnexionButton';
 import RegisterButton from 'src/components/Buttons/RegisterButton/RegisterButton';
@@ -11,67 +11,66 @@ import ProfileButton from '../Buttons/ProfileButton/ProfileButton';
 
 import './Header.scss';
 
-
 function Header() {
 
   const isLogged = useSelector((state) => state.user.isLogged);
   const firstname = JSON.parse(localStorage.getItem('user'));
 
-  
   return (
     <header className="header">
-      <section className="header__main__section">
-        {!isLogged && (
-          <>
-            <section className="header__section--left">
-              <Link to="/">
-                <img src={logo} alt="logo LinkO'Dev" className="header--logo" />
-              </Link>
-            </section>
-            <section className="header__section--right">
-              <div className="header--noLogged-buttons">
-                <NavLink
-                  to="/register"
-                ><RegisterButton />
-                </NavLink>
-                <NavLink
-                  to="/login"
-                ><ConnexionButton />
-                </NavLink>
-              </div>
-            </section>
-          </>
-        )}
-      </section>
-      {isLogged && (
-        <>
-          <section className="header__main__section">
-            <section className="header__section--left">
-              <Link to="/">
-                <img src={logo} alt="logo LinkO'Dev" className="header--logo" />
-              </Link>
-            </section>
-            <section className="header__section--right">
-              <div className="header--Logged-buttons">
-                <NavLink to="/me">
-                  <ProfileButton />
-                </NavLink>
-                <p className="header__Logged-message">Bienvenue {firstname}</p>
-              </div>
-            </section>
+        <section className="header__main__section">
+
+                    <section className="header__section--left">
+                    <Link to="/">
+                        <img src={logo} alt="logo LinkO'Dev" className="header--logo" />
+                    </Link>
+                    </section>
+
+            {isLogged && (
+                <>
+                    
+                    <section className="header__section--right">
+                        <div className="header--Logged-buttons">
+                            <NavLink to="/me">
+                            <ProfileButton />
+                            </NavLink>
+                            <p className="header__Logged-message">Bienvenue {firstname}</p>
+                        </div>
+                    </section>
+                    </>
+                )}
+
+            {!isLogged && (
+                <>
+                <section className="header__section--right">
+                    <div className="header--noLogged-buttons">
+                        <NavLink to="/register" >
+                            <RegisterButton />
+                        </NavLink>
+                        <NavLink to="/login" >
+                            <ConnexionButton />
+                        </NavLink>
+                    </div>
+                </section>  
+                </>
+            )}
+
           </section>
-          <section className="header__Loggedsection--center">
-            <div className="header__Logged-buttons--myposts">
-            <NavLink to="/me/posts">
-              <MyPosts  />
-              </NavLink>
-            </div>
-            <div className="header__Logged-buttons--logout">
-              <LogOutButton />
-            </div>
-          </section>
-        </>
-      )}
+          
+          {isLogged && (
+                <>
+              <section className="header__Loggedsection--center">
+                <div className="header__Logged-buttons--myposts">
+                  <NavLink to="/me/posts">
+                    <MyPosts  />
+                  </NavLink>
+                </div>
+                <div className="header__Logged-buttons--logout">
+                  <LogOutButton />
+                </div>
+              </section>
+              </>
+            )}
     </header>
   );
 }
