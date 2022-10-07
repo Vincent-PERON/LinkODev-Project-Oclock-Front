9b6f887cac2d7971f0ef594d316747786acd4d22
Vincent PERON <peronvincen@gmail.com>
2022-10-07 16:35:15 +0200

    style: 404 page in progress

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,15 +1,15 @@
+import errorPicture from 'src/assets/Images/404.png'; 
+
 import './Error.scss';
 
 function Error() {
   return (
     <div className="Error">
-      <h1 className="Error-title">404 !</h1>
-      <div className="Error-content">
-        <h2 className="Error-subtitle">La page que vous cherchez à été volée par</h2>
-        <h3 className="Error-subtitle2">Chuck Norris</h3>
-        <p className="Error-description">Nous n'osons pas lui demander de nous la restituer...</p>
-      </div>
+        <img className="Error-picture" src={errorPicture} alt="404-error" />
+
     </div>
+
+
   );
 }
 
