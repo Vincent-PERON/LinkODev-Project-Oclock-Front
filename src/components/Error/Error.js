5a84ffdecbc5bb3666023e629d306f7cf6b81960
Vincent PERON <peronvincen@gmail.com>
2022-10-10 11:00:29 +0200

    build: add 404 page with desktop style

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,14 +1,13 @@
+
 import './Error.scss';
 
 function Error() {
   return (
-    <div className="Error">
-      <h1 className="Error-title">404 !</h1>
-      <div className="Error-content">
-        <h2 className="Error-subtitle">La page que vous cherchez à été volée par</h2>
-        <h3 className="Error-subtitle2">Chuck Norris</h3>
-        <p className="Error-description">Nous n'osons pas lui demander de nous la restituer...</p>
-      </div>
+    <div className="Error grey">
+      <p className="Error--code">404</p>
+      <p className="Error--text">Il semble que vous vous soyez perdu !</p>
+      <p className="Error--text--end" >Un tour sur l'accueil devrait vous aider à retrouver votre route.</p>
+
     </div>
   );
 }
